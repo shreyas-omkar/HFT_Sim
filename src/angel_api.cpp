afe39637b44b060796625189035c34d2c7ff4113
Shreyas Hegde <shreyashegdeplus06@gmail.com>
2025-05-24 23:58:53 +0530

    Update angel_api.cpp

--- src/angel_api.cpp
+++ src/angel_api.cpp
@@ -5,17 +5,14 @@
 
 using json = nlohmann::json;
 
-// Replace these with your actual keys
 const std::string API_KEY = "gKyQKPXM ";
 const std::string SECRET_KEY = "c5ea77cb-146d-432e-9be8-71a84e108bec ";
-
 std::string generateJwtToken() {
     json body = {
         {"api_key", API_KEY},
         {"secret_key", SECRET_KEY}
     };
-
-    auto response = cpr::Post(
+auto response = cpr::Post(
         cpr::Url{"https://apiconnect.angelbroking.com/rest/auth/angelbroking/jwt/v1/generateTokens"},
         cpr::Header{
             {"Content-Type", "application/json"},
@@ -23,8 +20,7 @@ std::string generateJwtToken() {
         },
         cpr::Body{body.dump()}
     );
-
-    if (response.status_code != 200) {
+if (response.status_code != 200) {
         std::cerr << "Auth failed: " << response.status_code << "\n" << response.text << "\n";
         return "";
     }
@@ -32,7 +28,6 @@ std::string generateJwtToken() {
     json res = json::parse(response.text);
     return res["data"]["jwtToken"];
 }
-
 std::vector<double> fetchClosePrices(const std::string& symbolToken, const std::string& jwtToken) {
     std::string body = R"({
         "exchange": "NSE",
@@ -41,14 +36,13 @@ std::vector<double> fetchClosePrices(const std::string& symbolToken, const std::
         "fromdate": "2025-05-12 09:15",
         "todate": "2025-05-13 15:30"
     })";
-
-    auto response = cpr::Post(
+auto response = cpr::Post(
         cpr::Url{"https://apiconnect.angelbroking.com/rest/secure/angelbroking/historical/v1/getCandleData"},
         cpr::Header{
             {"Authorization", "Bearer " + jwtToken},
             {"Content-Type", "application/json"},
             {"Accept", "application/json"},
-            {"X-PrivateKey", API_KEY},  // Use the API key here
+            {"X-PrivateKey", API_KEY}, 
             {"X-SourceID", "WEB"},
             {"X-ClientLocalIP", "127.0.0.1"},
             {"X-ClientPublicIP", "127.0.0.1"},
@@ -61,7 +55,6 @@ std::vector<double> fetchClosePrices(const std::string& symbolToken, const std::
         std::cerr << "Failed to fetch data: " << response.status_code << "\n" << response.text << std::endl;
         return {};
     }
-
     json jsonData = json::parse(response.text);
     std::vector<double> prices;
 
